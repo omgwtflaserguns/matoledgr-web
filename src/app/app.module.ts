1781e11ae161144f1d42156028cb0555c6ec8b27
SchauerChristopher <git@7hw.de>
2018-02-16 06:39:37 +0100

    added two sub-components and started routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -14,16 +14,36 @@ import {
   , MatDialogModule
 } from '@angular/material';
 import { PayConfirmDialogComponent } from './pay-confirm-dialog/pay-confirm-dialog.component';
+import { LoginComponent } from './login/login.component';
+import { DrinksComponent } from './drinks/drinks.component';
+import { PagenotfoundComponent } from './pagenotfound/pagenotfound.component';
+
+const appRoutes: Routes = [
+  { path: 'drinks', component: DrinksComponent },
+  { path: 'login', component: LoginComponent },
+  { path: '',
+    redirectTo: '/login',
+    pathMatch: 'full'
+  },
+  { path: '**', component: PagenotfoundComponent }
+];
 
 @NgModule({
   declarations: [
     AppComponent,
-    PayConfirmDialogComponent
+    PayConfirmDialogComponent,
+    LoginComponent,
+    DrinksComponent,
+    PagenotfoundComponent
   ],
   entryComponents: [
     PayConfirmDialogComponent
   ],
   imports: [
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // <-- debugging purposes only
+    ),
     BrowserModule,
     MatButtonModule,
     MatToolbarModule,
