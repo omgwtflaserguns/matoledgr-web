96186593c275433d911a0ccb97b54ddc5acfd0b4
SchauerChristopher <git@7hw.de>
2018-02-17 20:43:41 +0100

    started methods for login but they route without reason

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -6,10 +6,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-  public benutzername = "";
-  public passwort = "";
+  public username = "";
+  public password = "";
 
   constructor() { }
+
+  public login(): void {
+    console.log("Would love to login with: ", this.username, " ", this.password);
+  }
+
+  public register(): void {
+    console.log("Would love to register with: ", this.username, " ", this.password);
+  }
 
   ngOnInit() {
   }
